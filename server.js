61bdedd216c6be9fca13a8b3a272b9fa253130e9
pbrain19 <pbrain19@gmail.com>
2015-08-13 19:06:31 -0400

    started the front page

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+
+app.use('/lib', express.static('bower_components'));
+app.use('/public', express.static('app'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
