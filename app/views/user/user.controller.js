230fdbf466bc06231f1976621cb9e14feb366502
Taylor Higgins <thiggins@Taylor-Higgins-2.local>
2015-09-03 19:29:41 -0400

    my first commit

---
+++ app/views/user/user.controller.js
@@ -0,0 +1,8 @@
+<script>
+angular.module('app', [])
+    .controller('userController', function ($scope) {
+      $scope.helloTo = {};
+      $scope.helloTo.title = 'World, AngularJS';
+
+});
+</script>
