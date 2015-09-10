08365551db61dfd4c1dad4013e1fb4fb0902aae0
T higgins <higgins.taylor.a@gmail.com>
2015-09-10 18:55:43 -0400

    added the dropdown

--- app/views/user/user.controller.js
+++ app/views/user/user.controller.js
@@ -1,8 +1,26 @@
-<script>
-angular.module('app', [])
-    .controller('userController', function ($scope) {
-      $scope.helloTo = {};
-      $scope.helloTo.title = 'World, AngularJS';
 
-});
-</script>
+angular.module("app")
+    .controller("userController", function ($scope) {
+
+  $scope.companyTypes = [
+    'Agency',
+    'Brand',
+    'Media Group',
+    'Other'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+
+ });
