1d161d8e71ad60678bc09e4bfaa1699fff7bfd55
Eleni Palmos <gkbluestocking@gmail.com>
2015-09-30 17:52:53 -0400

    scoped controller to model for select menu

--- app/views/goal/goal.controller.js
+++ app/views/goal/goal.controller.js
@@ -1,3 +1,16 @@
 angular.module('app').controller('goalController', function ($scope) {
 
+$scope.options = [
+  "traffic to site",
+  "product purchase",
+  "subscriber sign-up",
+  "brand awareness",
+  "other (redirected to SDR team)",
+  "seasonal campaign push (i.e., holiday)"
+];
+
+$scope.fakeModel = {
+  goal: $scope.options[2]
+}
+
 });
